c103fb5fc6714230d787b27233a13e9a01d3d7ad
rooodorei <2155536845@qq.com>
2025-02-07 21:21:54 +0800

    修改

--- helloworld-app/src/hello/helloworld.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
--- helloworld-app/src/hello/note.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-public class note {
-public static void main(String[] args) {
-    //hello
-    System.out.println("Hello, World!");
-    System.out.println(10+'a');
-}
-}
