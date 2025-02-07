20ff8bd9fbb81f5ce75b5ea87a167c6abdd745a9
rooodorei <2155536845@qq.com>
2025-02-07 15:59:07 +0800

    1

---
+++ helloworld-app/src/scanner/scanner.java
@@ -0,0 +1,12 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入：");
+        int num = sc.nextInt();
+        System.out.println("输出：" + num);
+    }
+}
